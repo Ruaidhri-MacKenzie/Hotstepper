5be72b0f381ddea8771f9f91993f3020edfa7cb4
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-05-04 19:44:49 +0100

    Added patch list and oscilloscope

--- src/synth/keyboard-synth.js
+++ src/synth/keyboard-synth.js
@@ -7,61 +7,97 @@ export default class KeyboardSynth extends Synth {
 		this.volume = 75;
 
 		// DOM REFERENCES
-		this.$oscillatorTypes = document.querySelectorAll("input[name='oscillatorType']");
+		this.$element = document.querySelector(".synth");
 
+		// Oscillator
+		this.$oscillatorTypes = document.querySelectorAll("input[name='oscillatorType']");
+		this.$oscilloscope = document.querySelector(".oscillator__oscilloscope");
+
+		// Frequency Filter
 		this.$filterTypes = document.querySelectorAll("input[name='filterType']");
 		this.$filterCutoff = document.querySelector(".filter__cutoff-input");
 		this.$filterCutoffPot = document.querySelector(".filter__cutoff-display");
 		this.$filterResonance = document.querySelector(".filter__resonance-input");
 		this.$filterResonancePot = document.querySelector(".filter__resonance-display");
 
+		// Low-Frequency Oscillator (LFO)
 		this.$lfoRate = document.querySelector(".lfo__rate-input");
 		this.$lfoRatePot = document.querySelector(".lfo__rate-display");
 		this.$lfoDepth = document.querySelector(".lfo__depth-input");
 		this.$lfoDepthPot = document.querySelector(".lfo__depth-display");
 
+		// Amplitude Envelope
 		this.$envelopeAttack = document.querySelector(".envelope__attack-input");
 		this.$envelopeDecay = document.querySelector(".envelope__decay-input");
 		this.$envelopeSustain = document.querySelector(".envelope__sustain-input");
 		this.$envelopeRelease = document.querySelector(".envelope__release-input");
 
-		this.$volume = document.querySelector(".master__volume");
-		this.$patchLoad = document.querySelector(".master__load");
-		this.$patchSave = document.querySelector(".master__save");
-		this.$patchName = document.querySelector(".master__patch-name");
+		// Master Synth Volume
+		this.$volume = document.querySelector(".master__volume-input");
+
+		// Synth Patches
+		this.$patchSave = document.querySelector(".master__patch-save");
+		this.$patchList = document.querySelector(".master__patch-list");
+
+		// Musical DOM Keyboard Keys
+		this.$keyboardKeys = document.querySelectorAll(".keyboard__key");
 
 		// EVENT HANDLERS
+		// Oscillator
 		this.$oscillatorTypes.forEach(($option) => $option.addEventListener("input", this.onChangeSelect.bind(this)));
 
+		// Frequency Filter
 		this.$filterTypes.forEach(($option) => $option.addEventListener("input", this.onChangeSelect.bind(this)));
 		this.$filterCutoff.addEventListener("input", this.onChangeInput.bind(this));
 		this.$filterCutoffPot.addEventListener("click", this.onClickPot.bind(this));
 		this.$filterResonance.addEventListener("input", this.onChangeInput.bind(this));
 		this.$filterResonancePot.addEventListener("click", this.onClickPot.bind(this));
 
+		// Low-Frequency Oscillator (LFO)
 		this.$lfoRate.addEventListener("input", this.onChangeInput.bind(this));
 		this.$lfoRatePot.addEventListener("click", this.onClickPot.bind(this));
 		this.$lfoDepth.addEventListener("input", this.onChangeInput.bind(this));
 		this.$lfoDepthPot.addEventListener("click", this.onClickPot.bind(this));
 
-		this.$envelopeAttack.addEventListener("input", this.onChangeInput.bind(this));
-		this.$envelopeDecay.addEventListener("input", this.onChangeInput.bind(this));
-		this.$envelopeSustain.addEventListener("input", this.onChangeInput.bind(this));
-		this.$envelopeRelease.addEventListener("input", this.onChangeInput.bind(this));
+		// Amplitude Envelope
+		this.$envelopeAttack.addEventListener("input", this.onChangeRange.bind(this));
+		this.$envelopeDecay.addEventListener("input", this.onChangeRange.bind(this));
+		this.$envelopeSustain.addEventListener("input", this.onChangeRange.bind(this));
+		this.$envelopeRelease.addEventListener("input", this.onChangeRange.bind(this));
 
+		// Master Synth Volume
 		this.$volume.addEventListener("input", this.onChangeVolume.bind(this));
-		this.$patchLoad.addEventListener("click", this.loadPatch.bind(this));
+
+		// Save Patch button
 		this.$patchSave.addEventListener("click", this.savePatch.bind(this));
 
 		// Musical QWERTY Keyboard
 		document.addEventListener("keydown", this.onKeyDown.bind(this));
 
+		// Musical DOM Keyboard Keys
+		this.$keyboardKeys.forEach(($key) => {
+			$key.addEventListener("mousedown", this.onClickKey.bind(this));
+		});
+
+		// INITIAL SETUP
 		// Set initial values of input elements from patch values
 		this.setInputValuesFromPatch();
+
+		// Get list of patches from localStorage and display in patch list
+		this.refreshPatchList();
+
+		// Start drawing oscilloscope
+		this.canvasContext = this.$oscilloscope.getContext("2d");
+		this.canvasContext.lineWidth = 2;
+		requestAnimationFrame(this.drawOscilloscope.bind(this));
 	}
 
-	onChangeVolume() {
+	onChangeVolume(event) {
 		this.volume = Number(this.$volume.value);
+	}
+
+	onChangeRange(event) {
+		this.patch[event.target.name] = Number(event.target.value);
 	}
 
 	onChangeInput(event) {
@@ -107,39 +143,65 @@ export default class KeyboardSynth extends Synth {
 		this.setPotPosition($display);
 	}
 
+	onClickKey(event) {
+		this.play(PITCH[event.target.dataset.pitch]);
+	}
+
 	setPotPosition($display) {
 		const $input = $display.parentNode.children[2];
 		$display.style.setProperty("--rotate", `${($input.value / $input.max) * 100 * 2 - 100}deg`);
 	}
 
 	onKeyDown(event) {
+		if (!this.$element.classList.contains("synth--active")) return;
+		if (event.ctrlKey) return;
 		if (event.repeat) return;
+
 		let pitch = PITCH.C4;
 		const key = event.key.toLowerCase();
 
-		if (key === "w") pitch = PITCH.C3;
-		else if (key === "e") pitch = PITCH.D3;
-		else if (key === "r") pitch = PITCH.E3;
-		else if (key === "t") pitch = PITCH.F3;
-		else if (key === "y") pitch = PITCH.G3;
-		else if (key === "u") pitch = PITCH.A3;
-		else if (key === "i") pitch = PITCH.B3;
-		else if (key === "s") pitch = PITCH.C4;
-		else if (key === "d") pitch = PITCH.D4;
-		else if (key === "f") pitch = PITCH.E4;
-		else if (key === "g") pitch = PITCH.F4;
-		else if (key === "h") pitch = PITCH.G4;
-		else if (key === "j") pitch = PITCH.A4;
-		else if (key === "k") pitch = PITCH.B4;
-		else if (key === "z") pitch = PITCH.C5;
-		else if (key === "x") pitch = PITCH.D5;
-		else if (key === "c") pitch = PITCH.E5;
-		else if (key === "v") pitch = PITCH.F5;
-		else if (key === "b") pitch = PITCH.G5;
-		else if (key === "n") pitch = PITCH.A5;
-		else if (key === "m") pitch = PITCH.B5;
-		else if (key === ",") pitch = PITCH.C6;
-		else return;
+		// Check if shift is held, if so increase pitches by an octave
+		if (event.shiftKey) {
+			if (key === "a") pitch = PITCH.C4;
+			else if (key === "w") pitch = PITCH.Db4;
+			else if (key === "s") pitch = PITCH.D4;
+			else if (key === "e") pitch = PITCH.Eb4;
+			else if (key === "d") pitch = PITCH.E4;
+			else if (key === "f") pitch = PITCH.F4;
+			else if (key === "t") pitch = PITCH.Gb4;
+			else if (key === "g") pitch = PITCH.G4;
+			else if (key === "y") pitch = PITCH.Ab4;
+			else if (key === "h") pitch = PITCH.A4;
+			else if (key === "u") pitch = PITCH.Bb4;
+			else if (key === "j") pitch = PITCH.B4;
+			else if (key === "k") pitch = PITCH.C5;
+			else return;
+		} else {
+			if (key === "a") pitch = PITCH.C3;
+			else if (key === "w") pitch = PITCH.Db3;
+			else if (key === "s") pitch = PITCH.D3;
+			else if (key === "e") pitch = PITCH.Eb3;
+			else if (key === "d") pitch = PITCH.E3;
+			else if (key === "f") pitch = PITCH.F3;
+			else if (key === "t") pitch = PITCH.Gb3;
+			else if (key === "g") pitch = PITCH.G3;
+			else if (key === "y") pitch = PITCH.Ab3;
+			else if (key === "h") pitch = PITCH.A3;
+			else if (key === "u") pitch = PITCH.Bb3;
+			else if (key === "j") pitch = PITCH.B3;
+			else if (key === "k") pitch = PITCH.C4;
+			else return;
+		}
+
+		// Light up DOM keyboard
+		this.$keyboardKeys.forEach(($key) => {
+			if (PITCH[$key.dataset.pitch] === pitch) {
+				$key.classList.add("keyboard__key--active");
+				setTimeout(() => {
+					$key.classList.remove("keyboard__key--active");
+				}, 500);
+			}
+		});
 
 		this.play(pitch);
 	}
@@ -168,16 +230,141 @@ export default class KeyboardSynth extends Synth {
 		this.$envelopeRelease.value = this.patch.envelopeRelease;
 
 		this.$volume.value = this.volume;
-		this.$patchName.textContent = this.patch.name || "<Untitled>";
 	}
 
-	loadPatch() {
-		const patchName = prompt("Enter patch name:");
+	savePatch(e) {
+		if (!this.patch.name) {
+			const name = prompt("Enter a patch name:");
+			if (!name) return;
+
+			this.patch.name = name;
+		}
+
+		super.savePatch();
+		this.refreshPatchList();
+		super.loadPatch(this.patch.name);
+	}
+
+	loadPatch(e) {
+		const $patchEntries = document.querySelectorAll(".patch-entry");
+		$patchEntries.forEach(($entry) => $entry.classList.remove("patch-entry--active"));
+		e.target.parentNode.classList.add("patch-entry--active");
+
+		const patchName = e.target.textContent;
 		super.loadPatch(patchName);
 		this.setInputValuesFromPatch();
 	}
 
+	renamePatch(e) {
+		const $patchName = e.target.parentNode.parentNode.children[0];
+		const currentName = $patchName.textContent;
+		const newName = prompt(`Enter a new name for this patch. Current name: ${currentName}`);
+		if (!newName) return;
+
+		const patches = JSON.parse(localStorage.getItem("PATCHES"));
+		patches[newName] = patches[currentName];
+		patches[newName].name = newName;
+		delete patches[currentName];
+
+		localStorage.setItem("PATCHES", JSON.stringify(patches));
+		$patchName.textContent = newName;
+	}
+
+	deletePatch(e) {
+		const patchName = e.target.parentNode.parentNode.children[0].textContent;
+		const check = confirm(`Are you sure you want to delete this patch? Patch name: ${patchName}`);
+		if (!check) return;
+
+		const patches = JSON.parse(localStorage.getItem("PATCHES"));
+		delete patches[patchName];
+
+		localStorage.setItem("PATCHES", JSON.stringify(patches));
+		this.refreshPatchList();
+	}
+
 	play(pitch) {
 		super.play(this.context.currentTime, 0.5, pitch, 0, this.volume);
+	}
+
+	refreshPatchList() {
+		// Empty patch list
+		while (this.$patchList.firstChild) {
+			this.$patchList.removeChild(this.$patchList.lastChild);
+		}
+
+		const patches = JSON.parse(localStorage.getItem("PATCHES"));
+		if (!patches) {
+			return;
+		}
+
+		Object.keys(patches).forEach((patchName) => {
+			const $patch = document.createElement("div");
+			$patch.classList.add("patch-entry");
+			if (patchName === this.patch.name) {
+				$patch.classList.add("patch-entry--active");
+			}
+
+			const $label = document.createElement("p");
+			$label.classList.add("patch-entry__label");
+			$label.textContent = patchName;
+			$label.addEventListener("click", this.loadPatch.bind(this));
+			$patch.appendChild($label);
+
+			const $buttons = document.createElement("div");
+			$buttons.classList.add("patch-entry__buttons");
+			$patch.appendChild($buttons);
+
+			const $rename = document.createElement("button");
+			$rename.classList.add("patch-entry__rename");
+			$rename.textContent = "R";
+			$rename.addEventListener("click", this.renamePatch.bind(this));
+			$buttons.appendChild($rename);
+
+			const $delete = document.createElement("button");
+			$delete.classList.add("patch-entry__delete");
+			$delete.textContent = "X";
+			$delete.addEventListener("click", this.deletePatch.bind(this));
+			$buttons.appendChild($delete);
+
+			this.$patchList.appendChild($patch);
+		});
+	}
+
+	drawOscilloscope() {
+		const width = this.canvasContext.canvas.width;
+		const height = this.canvasContext.canvas.height;
+		const scaleY = height / 256; // matches intervals in data between 0 and 255
+		let startX = 0;
+
+		const timeData = new Uint8Array(this.analyser.frequencyBinCount);
+		this.analyser.getByteTimeDomainData(timeData); // data returned between 0 and 255, 128 is midpoint (no sound)
+
+		this.canvasContext.fillStyle = "rgba(0, 0, 0, 0.1)";
+		this.canvasContext.fillRect(0, 0, width, height);
+
+		this.canvasContext.strokeStyle = "rgb(0, 200, 0)";
+		this.canvasContext.beginPath();
+
+		// find the first point which has timeData less than or equal to 128
+		while (timeData[startX] > 128 && startX <= width) {
+			startX += 1;
+		}
+		if (startX >= width) startX = 0;
+
+		// find the first point which has timeData more than 128
+		while (timeData[startX] <= 128 && startX <= width) {
+			startX += 1;
+		}
+		if (startX >= width) startX = 0;
+		// these loops will find the first point at which the wave is rising in the timeData
+
+		for (let x = 0; x < width; x++) {
+			if (startX + x >= timeData.length) break;
+			const y = timeData[startX + x] * scaleY;
+			this.canvasContext.lineTo(x, height - y);
+		}
+
+		this.canvasContext.stroke();
+		requestAnimationFrame(this.drawOscilloscope.bind(this));
 	}
 }
